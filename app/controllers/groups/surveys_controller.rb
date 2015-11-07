c0a69da42a4c00ba459ae3de89386c57f9bcffcb
Anthony Laibe <anthony@laibe.cc>
2015-11-07 21:52:36 +0000

    Add gemfile form validation

--- app/controllers/groups/surveys_controller.rb
+++ app/controllers/groups/surveys_controller.rb
@@ -2,7 +2,7 @@ module Groups
   class SurveysController < Groups::BaseController
     def show
       survey  = ::Surveys::FindByCodeService.new(group.id, params[:id]).call
-      stats   = Surveys::GenerateStatsService.new(survey.id).call
+      stats   = ::Surveys::GenerateStatsService.new(survey.id).call
 
       @context = Surveys::ShowContext.new(survey, stats)
     end
--- app/forms/gemfile_form.rb
+++ app/forms/gemfile_form.rb
@@ -4,4 +4,6 @@ class GemfileForm < Reform::Form
 
   validates :owner_name, presence: true
   validates :document, presence: true
+
+  validates :document, file_size: { less_than: 2.megabytes }, file_content_type: { allow: ['text/plain'] }
 end
