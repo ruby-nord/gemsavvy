ec57f40a3e1f51cc84e926a509f52ceefa55db5d
Anthony Laibe <anthony@laibe.cc>
2015-11-08 14:44:18 +0000

    Add rails rumble js

---
+++ app/assets/javascripts/rails_rumble_config.js
@@ -0,0 +1,5 @@
+var rumbleBannerBackgroundColor, rumbleBannerTextColor, rumbleBannerLinkColor, rumbleBannerPlacement;
+rumbleBannerBackgroundColor = "#161616";
+rumbleBannerTextColor = "#b0b0b0";
+rumbleBannerLinkColor = "#ffffff";
+rumbleBannerPlacement = "bottom";
