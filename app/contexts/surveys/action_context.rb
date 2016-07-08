e66e9163f69b0d8fe2c4bc0554b601946b7897b3
Cecile Veneziani <contact@cecilitse.org>
2016-07-08 16:32:00 +0200

    Fix Survey edition page
    Update Survey action context:
    
    - Expose the Survey form
    - Add missing delegation of `when_manager` to presenter
    - Add missing delegation of `slug` to group

--- app/contexts/surveys/action_context.rb
+++ app/contexts/surveys/action_context.rb
@@ -3,23 +3,28 @@ module Surveys
 
     private
 
-    attr_reader :survey, :group
+    attr_reader :group, :survey
 
     public
+
+    attr_reader :form
 
     delegate :logo_url,       to: :group, prefix: true
     delegate :manager_email,  to: :group, prefix: true
     delegate :name,           to: :group, prefix: true
+    delegate :slug,           to: :group, prefix: true
     delegate :website_url,    to: :group, prefix: true
 
     delegate :when_group_has_website_url, to: :group_presenter
+    delegate :when_manager,               to: :group_presenter
 
     delegate :name, to: :survey, prefix: true
     delegate :code, to: :survey, prefix: true
 
-    def initialize(group, survey)
-      @survey = survey
+    def initialize(group, form)
+      @form   = form
       @group  = group
+      @survey = form.model
     end
 
     private
