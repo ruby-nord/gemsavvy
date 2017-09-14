a4eb60f056498ac506749906381b200604697bdc
Cecile Veneziani <contact@cecilitse.org>
2017-09-14 23:45:39 +0200

    Fix migrations due to switch from paperclip to carrierwave

--- db/migrate/20151107010610_create_groups.rb
+++ db/migrate/20151107010610_create_groups.rb
@@ -1,12 +1,12 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.string      :name
-      t.string      :manager_email
-      t.string      :manager_token
-      t.string      :slug
-      t.string      :website_url
-      t.attachment  :logo
+      t.string :name
+      t.string :manager_email
+      t.string :manager_token
+      t.string :slug
+      t.string :website_url
+      t.string :logo
 
       t.timestamps null: false
     end
--- db/migrate/20151107180753_add_column_logo.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnLogo < ActiveRecord::Migration
-  def change
-    add_column :groups, :logo, :string
-  end
-end
